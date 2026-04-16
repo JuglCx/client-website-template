891832426908d99d2b3edf028a2b4579300cfa0e
Madhan <madhan.raj@jugl.com>
2026-04-16 12:58:07 +0530

    bug fix

---
+++ src/lib/loadConfig.tsx
@@ -0,0 +1,28 @@
+const configFiles = import.meta.glob("../*/{api-config,chatbot-config,site-config}.json");
+
+export async function loadConfigs(brand: string) {
+  const loadJson = async (file: string) => {
+    const loader = configFiles[file];
+
+    if (!loader) {
+      throw new Error(`Config file not found: ${file}`);
+    }
+
+    const mod: any = await loader();
+    return mod.default;
+  };
+
+  const basePath = `../${brand}`;
+
+  const [apiConfig, chatbotConfig, siteConfig] = await Promise.all([
+    loadJson(`${basePath}/api-config.json`),
+    loadJson(`${basePath}/chatbot-config.json`),
+    loadJson(`${basePath}/site-config.json`),
+  ]);
+
+  return {
+    apiConfig,
+    chatbotConfig,
+    siteConfig,
+  };
+}
